70fd52c6b4064601b7a18835dbb52f95712d2f04
Juanchy Quintana <quintanajuanchy25@gmail.com>
2023-11-25 14:20:31 -0300

    app funcionando

---
+++ js/main.js
@@ -0,0 +1,178 @@
+class Persona {
+  constructor(nombre, edad, dni, sexo, anoNacimiento) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.dni = dni;
+    this.sexo = sexo;
+    this.anoNacimiento = anoNacimiento;
+  }
+
+  // Metodos
+  mostrarGeneracion() {
+    const anio = this.anoNacimiento;
+    let generacion = "";
+    let rasgos = "";
+
+    if (anio >= 1994) {
+      generacion = "Generación Z";
+      rasgos = "Irrevencia";
+    } else if (anio >= 1981) {
+      generacion = "Generación Y";
+      rasgos = "Frustración";
+    } else if (anio >= 1969) {
+      generacion = "Generación X";
+      rasgos = "Obsesión por el éxito";
+    } else if (anio >= 1949) {
+      generacion = "Generación Baby Boom";
+      rasgos = "Ambición";
+    } else {
+      generacion = "Generación Silent Generation";
+      rasgos = "Austeridad";
+    }
+
+    const seccionPrincipal = document.querySelector("#seccionPrincipal");
+    const mostrarGeneracion = document.createElement("div");
+    mostrarGeneracion.className = "d-flex flex-column justify-content-center";
+    mostrarGeneracion.innerHTML = `
+        <div class="d-flex flex-column justify-content-center">
+          <h2 class="fw-bold text-uppercase text-center">Rolling Prediction - Conozca su Generación</h2>
+
+            <div class="container text-center">
+              <p class="fs-4 fw-bold text-uppercase text-success">- Hola ${this.nombre} -</p>
+              <p class="text-uppercase fw-bold ">Te queriamos informar que perteneces a la ${generacion} y su rasgo caracteristico es ${rasgos}</p>
+          </div>
+
+          <div class="d-flex flex-column justify-content-center gap-2">
+            <button id="consultarDatos" class="btn btn-primary mt-2">Ver datos</button>
+
+            <button id="consultarEdad" class="btn btn-warning mt-2">¿Soy mayor de edad?</button>
+          </div>
+        </div>
+      `;
+    seccionPrincipal.appendChild(mostrarGeneracion);
+  }
+
+  esMayorDeEdad() {
+    if (this.edad >= 18) {
+      const seccionPrincipal = document.querySelector("#seccionPrincipal");
+      const mayorEdad = document.createElement("div");
+      mayorEdad.className = "d-flex flex-column justify-content-center";
+      mayorEdad.innerHTML = `
+          <div class="d-flex flex-column justify-content-center">
+            <h2 class="fw-bold text-uppercase text-center">Rolling Prediction - Conozca su Generación</h2>
+
+              <div class="container text-center">
+                <p class="fs-4 fw-bold text-uppercase text-success">- Hola ${this.nombre} -</p>
+                <p class="text-uppercase fw-bold ">Eres mayor de edad (${this.edad})</p>
+            </div>
+          </div>
+        `;
+      seccionPrincipal.appendChild(mayorEdad);
+    } else {
+      const seccionPrincipal = document.querySelector("#seccionPrincipal");
+      const menorEdad = document.createElement("div");
+      menorEdad.className = "d-flex flex-column justify-content-center";
+      menorEdad.innerHTML = `
+          <div class="d-flex flex-column justify-content-center">
+            <h2 class="fw-bold text-uppercase text-center">Rolling Prediction - Conozca su Generación</h2>
+
+              <div class="container text-center">
+                <p class="fs-4 fw-bold text-uppercase text-success">- Hola ${this.nombre} -</p>
+                <p class="text-uppercase fw-bold ">Eres menor de edad (${this.edad})</p>
+            </div>
+          </div>
+        `;
+      seccionPrincipal.appendChild(menorEdad);
+    }
+  }
+
+  mostrarDatos() {
+    const seccionPrincipal = document.querySelector("#seccionPrincipal");
+    const divResultado = document.createElement("div");
+    divResultado.className = "d-flex flex-column justify-content-center";
+    divResultado.innerHTML = `
+          <div class="d-flex flex-column justify-content-center">
+            <h2 class="fw-bold text-uppercase text-center">Rolling Prediction - Conozca su Generación</h2>
+
+              <div class="container text-center">
+                <p class="fs-4 fw-bold text-uppercase text-success">- Hola ${this.nombre} -</p>
+                <p class="text-uppercase fw-bold ">Estos son todos su datos:</p>
+
+                <ul class="list-unstyled">
+                  <li><span class="fw-bolder">Nombre:</span> ${this.nombre}</li>
+                  <li><span class="fw-bolder">Edad:</span> ${this.edad}</li>
+                  <li><span class="fw-bolder">D.N.I:</span> ${this.dni}</li>
+                  <li><span class="fw-bolder">Sexo:</span> ${this.sexo}</li>
+                  <li><span class="fw-bolder">Fecha></span> Nacimiento: ${this.anoNacimiento}</li>
+                </ul>
+            </div>
+          </div>
+        `;
+    seccionPrincipal.appendChild(divResultado);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Seleccionamos los inputs del interfaz para obtener el valor
+  const nombre = document.querySelector("#nombre");
+  const edad = document.querySelector("#edadPersona");
+  const dni = document.querySelector("#dni");
+  const sexo = document.querySelector("#sexo");
+  const nacimiento = document.querySelector("#fechaNacimiento");
+
+  // Seleccionamos los elementos del DOM
+  const formulario = document.querySelector("#formGeneracion");
+  const zonaAlerta = document.querySelector("#zoneAlerta");
+  const btnSubmit = document.querySelector('#formGeneracion button[type="submit"]');
+  const btnEdad = document.querySelector('#consultarEdad')
+  const btnDatos = document.querySelector('#consultarDatos')
+
+  // Eventos del Formulario
+  formulario.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if (
+      nombre.value.trim() === "" ||
+      edad.value.trim() === "" ||
+      dni.value.trim() === "" ||
+      sexo.value.trim() === "" ||
+      nacimiento.value.trim() === ""
+    ) {
+      mostrarAlerta();
+    } else if (btnSubmit.innerHTML.includes("Mostrar Generación")) {
+      const persona = new Persona(
+        nombre.value,
+        edad.value,
+        dni.value,
+        sexo.value,
+        nacimiento.value
+      );
+
+      formulario.className = "d-none";
+
+      persona.mostrarGeneracion();
+    } else {
+      const alert = document.querySelector("#alertaFormulario");
+      alert.className = "d-none";
+      alert.innerHTML = "";
+    }
+  });
+
+  // Funciones
+  let alertaMostrada = false;
+  const mostrarAlerta = () => {
+    if (!alertaMostrada) {
+      const alert = document.createElement("div");
+      alert.id = "alertaFormulario";
+      alert.className = "d-flex flex-column justify-content-center";
+      alert.innerHTML = `        
+      <div class="d-flex flex-column justify-content-center">
+          <p class="text-white text-center fw-bold bg-danger rounded mt-2 shadow ">Hay campos vacíos. Complete todo, por favor.</p>
+      </div>
+  
+      `;
+      zonaAlerta.appendChild(alert);
+      alertaMostrada = true;
+    }
+  };
+});
